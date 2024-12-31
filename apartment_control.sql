90e6ee2d09c65aa92e24265efd352fbdd2624e08
ntabodoiqua <135292057+ntabodoiqua@users.noreply.github.com>
2024-12-31 09:07:13 +0700

    update database

--- apartment_control.sql
+++ apartment_control.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 22, 2024 lúc 05:57 PM
+-- Thời gian đã tạo: Th12 31, 2024 lúc 03:05 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `apartment_control`
 --
+CREATE DATABASE IF NOT EXISTS `apartment_control` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `apartment_control`;
 
 -- --------------------------------------------------------
 
@@ -40,7 +42,7 @@ CREATE TABLE `admin_table` (
 --
 
 INSERT INTO `admin_table` (`admin_id`, `admin_name`, `admin_email`, `admin_username`, `admin_password`) VALUES
-(1, 'Nguyễn Thế Anh', 'ntabodoiqua1@gmail.com', 'ntabodoiqua', '$2y$10$F4FeQyrcZQ7/XXfPG6HmUOMY71UiXAFFT8jDoaW4uPwwsAID789Q2');
+(1, 'Nguyễn Thế Anh', 'ntabodoiqua1@gmail.com', 'ntabodoiqua', '$2y$10$XQ1pHtfANnZRB8bUbi5C2ekbNunUGXZOZdBt7JVtLNPS.ABgI6dsq');
 
 -- --------------------------------------------------------
 
@@ -55,17 +57,21 @@ CREATE TABLE `apartments` (
   `apartment_area` int(11) NOT NULL,
   `owner_id` int(11) NOT NULL,
   `curr_living` int(11) NOT NULL,
-  `apartment_type` varchar(50) NOT NULL
+  `is_left` tinyint(1) NOT NULL,
+  `apartment_type` varchar(50) NOT NULL,
+  `apartment_ngaylap` date DEFAULT NULL,
+  `apartment_ngayroi` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `apartments`
 --
 
-INSERT INTO `apartments` (`apartment_id`, `apartment_name`, `apartment_num`, `apartment_area`, `owner_id`, `curr_living`, `apartment_type`) VALUES
-(2, 'Nguyễn Thế Anh', 101, 120, 3, 2, 'Nhà thường'),
-(3, 'Phạm Văn Cường', 201, 150, 9, 1, 'Nhà thường'),
-(4, 'Phạm Văn Thanh', 202, 111, 10, 2, 'Nhà thường');
+INSERT INTO `apartments` (`apartment_id`, `apartment_name`, `apartment_num`, `apartment_area`, `owner_id`, `curr_living`, `is_left`, `apartment_type`, `apartment_ngaylap`, `apartment_ngayroi`) VALUES
+(1, 'Phòng 101 - Nguyễn Thế Anh', 101, 20, 1, 4, 0, 'Nhà thường', '2024-12-25', NULL),
+(2, 'Phòng 201 - Vũ Đức Hiếu', 201, 25, 2, 3, 0, 'Penhouse', '2024-12-25', NULL),
+(3, 'Phòng 301 - Trần Hữu Đạt', 301, 27, 3, 2, 0, 'Nhà thường', '2024-12-25', NULL),
+(4, 'Phòng 401 - Trần Dần', 401, 31, 0, 0, 1, 'Nhà thường', '2024-12-26', '2024-12-30');
 
 -- --------------------------------------------------------
 
@@ -85,44 +91,27 @@ CREATE TABLE `apartment_fees` (
 --
 
 INSERT INTO `apartment_fees` (`apartment_fee_id`, `apartment_id`, `fee_id`, `money`) VALUES
-(1, 2, 2, 1920000),
-(2, 3, 2, 2400000),
-(3, 4, 2, 1776000),
-(4, 2, 3, 1920000),
-(5, 3, 3, 2400000),
-(6, 4, 3, 1776000),
-(7, 2, 4, 840000),
-(8, 3, 4, 1050000),
-(9, 4, 4, 777000),
-(18, 2, 11, 140000),
-(19, 3, 11, 70000),
-(20, 4, 11, 140000),
-(21, 2, 12, 1920000),
-(22, 3, 12, 2400000),
-(23, 4, 12, 1776000),
-(24, 2, 13, 100000),
-(25, 3, 13, 100000),
-(26, 4, 13, 100000),
-(27, 2, 14, 120000),
-(28, 3, 14, 120000),
-(29, 4, 14, 120000),
-(30, 2, 16, 115000),
-(31, 3, 16, 115000),
-(32, 4, 16, 115000),
-(34, 3, 17, 70000),
-(35, 4, 17, 140000),
-(36, 2, 18, 140000),
-(37, 3, 18, 70000),
-(38, 4, 18, 140000),
-(39, 2, 19, 140000),
-(40, 3, 19, 70000),
-(41, 4, 19, 140000),
-(42, 2, 20, 140000),
-(43, 3, 20, 70000),
-(44, 4, 20, 140000),
-(45, 2, 21, 1980000),
-(46, 3, 21, 2475000),
-(47, 4, 21, 1831500);
+(6, 1, 5, 70000),
+(7, 2, 5, 70000),
+(8, 3, 5, 70000),
+(9, 1, 6, 1200000),
+(10, 2, 6, 2400000),
+(11, 3, 6, 2400000),
+(12, 1, 7, 320000),
+(13, 2, 7, 400000),
+(14, 3, 7, 432000),
+(15, 1, 8, 100000),
+(16, 1, 9, 320000),
+(17, 2, 9, 400000),
+(18, 3, 9, 432000),
+(19, 1, 10, 320000),
+(20, 2, 10, 400000),
+(21, 3, 10, 432000),
+(22, 2, 11, 2400002),
+(23, 3, 11, 2400002),
+(24, 1, 12, 140000),
+(25, 2, 12, 175000),
+(26, 3, 12, 189000);
 
 -- --------------------------------------------------------
 
@@ -145,20 +134,14 @@ CREATE TABLE `fees` (
 --
 
 INSERT INTO `fees` (`fee_id`, `type_id`, `fee_additional_amount`, `fee_name`, `fee_image`, `fee_start_date`, `fee_due_date`) VALUES
-(1, 1, 0, 'Phí dịch vụ chung cư Tháng 12/2024', 'elon.jpg', '2024-12-01', '2024-12-31'),
-(2, 1, 0, 'Phí dịch vụ chung cư Tháng 12/2024', 'elon.jpg', '2024-12-01', '2024-12-31'),
-(3, 1, 0, 'Phí dịch vụ chung cư Tháng 1/2025', 'dell.png', '2025-01-01', '2025-01-31'),
-(4, 2, 0, 'Phí quản lý chung cư tháng 12/2024', 'lenovo_logo.png', '2024-12-01', '2024-12-30'),
-(11, 5, 0, 'Phí gửi xe máy tháng 12/2024', '747.png', '2024-12-01', '2024-12-31'),
-(12, 1, 0, 'Phí dịch vụ chung cư Tháng 2/2025', 'customer1.jpg', '2025-02-01', '2025-02-28'),
-(13, 3, 100000, 'Phí biển đảo tháng 12/2024', 'dohoa.png', '2024-12-01', '2024-12-31'),
-(14, 4, 120000, 'Phí vì người nghèo tháng 12/2024', 'hp_logo.png', '2024-12-01', '2024-12-31'),
-(15, 6, 0, 'Phí gửi Oto tháng 1/2025', 'lenovo_logo.png', '2025-01-01', '2025-01-31'),
-(16, 3, 115000, 'Đóng góp vì người nghèo tháng 1/2025', 'msi_logo.png', '2025-01-01', '2025-01-31'),
-(18, 5, 0, 'Phí gửi xe máy tháng 12/2024', 'lenovo_logo.png', '2024-12-01', '2024-12-31'),
-(19, 5, 0, 'Phí gửi xe máy tháng 1/2025', 'slide7.jpg', '2025-01-01', '2025-01-31'),
-(20, 5, 0, 'Phí gửi xe máy tháng 12/2023', 'QR_code.jpg', '2023-12-01', '2023-12-31'),
-(21, 1, 0, 'Phí dịch vụ chung cư Tháng 3/2025', 'poster.jpg', '2025-03-01', '2025-03-31');
+(5, 5, 0, 'Phí gửi xe máy tháng 12/2024', 'Phi_xe_may.png', '2024-12-01', '2024-12-31'),
+(6, 6, 0, 'Phí gửi xe Oto tháng 12/2024', 'Phi_oto.png', '2024-12-01', '2024-12-31'),
+(7, 1, 0, 'Phí dịch vụ chung cư Tháng 12/2024', 'Phi_dich_vu_chung_cu.png', '2024-12-01', '2024-12-31'),
+(8, 4, 100000, 'Tiền điện tháng 12/2024', 'Phi_tien_ich.png', '2024-12-01', '2024-12-31'),
+(9, 1, 0, 'Phí dịch vụ chung cư Tháng 2/2025', 'Phi_dich_vu_chung_cu.png', '2025-02-01', '2025-02-28'),
+(10, 1, 0, 'Phí dịch vụ chung cư tháng 11/2024', 'Phi_dich_vu_chung_cu.png', '2024-11-01', '2024-11-30'),
+(11, 6, 0, 'Phí gửi xe Oto tháng 12/2023', 'Phi_oto.png', '2023-12-01', '2023-12-31'),
+(12, 2, 0, 'Phí quản lý chung cư tháng 1/2025', 'Phi_quan_ly_chung_cu.png', '2025-01-01', '2025-01-31');
 
 -- --------------------------------------------------------
 
@@ -172,7 +155,7 @@ CREATE TABLE `payments` (
   `apartment_id` int(11) NOT NULL,
   `amount_due` double NOT NULL,
   `amount_paid` double NOT NULL,
-  `payment_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `payment_date` datetime DEFAULT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -181,24 +164,20 @@ CREATE TABLE `payments` (
 --
 
 INSERT INTO `payments` (`payment_id`, `fee_id`, `apartment_id`, `amount_due`, `amount_paid`, `payment_date`, `status`) VALUES
-(1, 12, 2, 1920000, 1920000, '2024-12-21 16:01:48', 'Thanh toán 1 phần'),
-(2, 12, 3, 2400000, 2400000, '2024-12-21 16:05:49', 'Đã thanh toán'),
-(3, 12, 4, 1776000, 1776000, '2024-12-21 16:07:42', 'Đã thanh toán'),
-(4, 16, 2, 115000, 0, '2024-12-21 16:27:44', 'Chưa thanh toán'),
-(5, 16, 3, 115000, 115000, '2024-12-21 16:28:01', 'Đã thanh toán'),
-(6, 16, 4, 115000, 0, '2024-12-21 16:27:44', 'Chưa thanh toán'),
-(10, 18, 2, 140000, 100000, '2024-12-21 17:37:45', 'Thanh toán 1 phần'),
-(11, 18, 3, 70000, 0, '2024-12-21 17:19:26', 'Chưa thanh toán'),
-(12, 18, 4, 140000, 0, '2024-12-21 17:19:26', 'Chưa thanh toán'),
-(13, 19, 2, 140000, 0, '2024-12-21 17:24:24', 'Chưa thanh toán'),
-(14, 19, 3, 70000, 0, '2024-12-21 17:24:24', 'Chưa thanh toán'),
-(15, 19, 4, 140000, 0, '2024-12-21 17:24:24', 'Chưa thanh toán'),
-(16, 20, 2, 140000, 0, '2024-12-21 17:25:17', 'Chưa thanh toán'),
-(17, 20, 3, 70000, 0, '2024-12-21 17:25:17', 'Chưa thanh toán'),
-(18, 20, 4, 140000, 0, '2024-12-21 17:25:17', 'Chưa thanh toán'),
-(19, 21, 2, 1980000, 0, '2024-12-22 08:35:52', 'Chưa thanh toán'),
-(20, 21, 3, 2475000, 0, '2024-12-22 08:35:52', 'Chưa thanh toán'),
-(21, 21, 4, 1831500, 0, '2024-12-22 08:35:52', 'Chưa thanh toán');
+(6, 5, 1, 70000, 70000, '2024-12-26 10:02:36', 'Đã thanh toán'),
+(7, 5, 2, 70000, 70000, '2024-12-28 19:36:31', 'Đã thanh toán'),
+(9, 6, 1, 1200000, 1200000, '2024-12-28 19:36:41', 'Đã thanh toán'),
+(11, 6, 3, 2400000, 2400000, '2024-12-28 19:37:10', 'Đã thanh toán'),
+(15, 8, 1, 100000, 100000, '2024-12-28 19:37:00', 'Đã thanh toán'),
+(17, 9, 2, 400000, 400000, '2024-12-28 19:36:49', 'Đã thanh toán'),
+(32, 10, 1, 320000, 320000, '2024-11-27 10:26:11', 'Đã thanh toán'),
+(33, 10, 2, 400000, 400000, '2024-11-28 10:26:56', 'Đã thanh toán'),
+(34, 10, 3, 432000, 432000, '2024-11-13 10:27:19', 'Đã thanh toán'),
+(35, 11, 2, 2400002, 2400002, '2023-12-27 10:29:09', 'Đã thanh toán'),
+(36, 11, 3, 2400002, 2400002, '2023-12-28 10:29:39', 'Đã thanh toán'),
+(37, 12, 1, 140000, 140000, '2024-12-30 10:48:07', 'Đã thanh toán'),
+(38, 12, 2, 175000, 0, NULL, 'Chưa thanh toán'),
+(39, 12, 3, 189000, 0, NULL, 'Chưa thanh toán');
 
 -- --------------------------------------------------------
 
@@ -214,6 +193,8 @@ CREATE TABLE `residents` (
   `apartment_id` int(11) NOT NULL,
   `resident_image` varchar(255) NOT NULL,
   `resident_status` varchar(50) NOT NULL,
+  `resident_ngayden` date DEFAULT NULL,
+  `resident_ngaydi` date DEFAULT NULL,
   `resident_dob` date NOT NULL,
   `resident_relation_owner` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -222,14 +203,19 @@ CREATE TABLE `residents` (
 -- Đang đổ dữ liệu cho bảng `residents`
 --
 
-INSERT INTO `residents` (`resident_id`, `resident_name`, `resident_phone`, `resident_email`, `apartment_id`, `resident_image`, `resident_status`, `resident_dob`, `resident_relation_owner`) VALUES
-(1, 'Vũ Hồng An', '0966277109', 'honganh@gmail.com', 2, 'customer3.jpg', 'Tạm vắng', '0000-00-00', 'Vợ'),
-(2, 'Anh Đức', '123456789', 'honganh@gmail.com', 2, 'customer2.jpg', 'Đã chuyển đi', '0000-00-00', 'Vợ'),
-(3, 'Vũ Thị Gái', '123', 'dsad', 2, 'ceonvdia.jpg', 'Đang sống', '2021-01-28', 'Chủ hộ'),
-(9, 'Phạm Văn Cường', '111', 'dsaddsda', 3, 'elon.jpg', 'Đang sống', '2024-12-05', 'Chủ hộ'),
-(10, 'Phạm Văn Thanh', '1023', '424', 4, 'QR_code.jpg', 'Đang sống', '2024-08-01', 'Chủ hộ'),
-(11, 'Đức Quý', '452', '45254', 4, 'sontung.jpg', 'Đang sống', '2024-08-30', 'Em trai ruột'),
-(15, 'Nguyễn Văn Quốc Việt', '034204004454', 'thuthucallme@gmail.com', 2, 'camung_icon.png', 'Tạm trú', '2024-04-30', 'Không có');
+INSERT INTO `residents` (`resident_id`, `resident_name`, `resident_phone`, `resident_email`, `apartment_id`, `resident_image`, `resident_status`, `resident_ngayden`, `resident_ngaydi`, `resident_dob`, `resident_relation_owner`) VALUES
+(1, 'Nguyễn Thế Anh', '20224921', 'anhnta2004@gmail.com', 1, 'theanh.png', 'Đang sống', '2024-12-25', NULL, '2004-07-30', 'Chủ hộ'),
+(2, 'Vũ Đức Hiếu', '20210314', 'duchieu@gmail.com', 2, 'vuhieu.png', 'Đang sống', '2024-12-25', NULL, '2003-01-01', 'Chủ hộ'),
+(3, 'Trần Hữu Đa', '20225120', 'huudat@gmail.com', 3, 'nguoidan_7.jpg', 'Đang sống', '2024-12-25', NULL, '2004-01-01', 'Chủ hộ'),
+(4, 'Lionel Messi', '20220000', 'messi@gmail.com', 1, 'nguoidan_9.jpg', 'Tạm vắng', '2024-12-26', NULL, '1987-06-24', 'Con ruột'),
+(5, 'Trần Hà Linh', '20220001', 'halinh@gmail.com', 2, 'nguoidan_1.jpg', 'Đang sống', '2024-12-25', NULL, '1999-01-01', 'Vợ'),
+(6, 'Nguyễn Thị An Nhiên', '20220002', 'annhien@gmail.com', 3, 'nguoidan_2.jpg', 'Đã chuyển đi', '2024-12-25', '2024-12-26', '1998-01-01', 'Vợ'),
+(7, 'Phạm Gia Linh', '20220003', 'gialinh@gmail.com', 1, 'nguoidan_3.jpg', 'Đang sống', '2024-12-25', NULL, '1995-12-30', 'Con ruột'),
+(8, 'Hoàng Hạ Vy', '20220004', 'havi@gmail.com', 2, 'nguoidan_4.jpg', 'Đang sống', '2024-12-25', NULL, '1994-01-01', 'Con ruột'),
+(9, 'Đặng Thị Hoàng Anh', '20220005', 'hoanganh@gmail.com', 3, 'nguoidan_5.jpg', 'Đang sống', '2024-12-25', NULL, '1994-01-01', 'Con ruột'),
+(10, 'Vũ Khánh Linh', '20220006', 'khanhlinh@gmail.com', 1, 'nguoidan_6.jpg', 'Đang sống', '2024-12-25', NULL, '1990-01-01', 'Vợ'),
+(11, 'Trần Dần', '20220007', 'trandan@gmail.com', 4, 'nguoidan_12.jpg', 'Đã chuyển đi', '2024-12-26', '2024-12-26', '1980-01-01', 'Chủ hộ'),
+(12, 'Trần Đức Bo', '20229999', 'ducbo@gmail.com', 1, 'nguoidan_14.jpg', 'Tạm trú', NULL, NULL, '1955-01-01', 'Không có');
 
 -- --------------------------------------------------------
 
@@ -251,7 +237,7 @@ CREATE TABLE `tamtru` (
 --
 
 INSERT INTO `tamtru` (`tamtru_id`, `resident_id`, `lydo`, `tamtru_thuongtru`, `start_date`, `end_date`) VALUES
-(1, 15, 'Ở trọ', 'Hạ Đình', '2024-07-30', '2029-07-30');
+(1, 12, 'Đi học Đại học', 'Thụy Chính', '2025-01-01', '2029-01-01');
 
 -- --------------------------------------------------------
 
@@ -274,7 +260,7 @@ CREATE TABLE `tamvang` (
 --
 
 INSERT INTO `tamvang` (`tamvang_id`, `resident_id`, `cccd`, `lydo`, `tamvang_address`, `start_date`, `end_date`) VALUES
-(1, 1, '0966277109', 'Đi học Đại học', 'Hà Nội', '2025-07-30', '2029-07-30');
+(1, 4, '20220000', 'Đi công tác', 'Singapore', '2025-01-01', '2029-01-01');
 
 -- --------------------------------------------------------
 
@@ -293,12 +279,12 @@ CREATE TABLE `type_fee` (
 --
 
 INSERT INTO `type_fee` (`type_id`, `type_name`, `type_rate`) VALUES
-(1, 'Phí dịch vụ chung cư', 16500),
+(1, 'Phí dịch vụ chung cư', 16000),
 (2, 'Phí quản lý chung cư', 7000),
 (3, 'Phí không bắt buộc', 0),
 (4, 'Phí tiện ích', 0),
 (5, 'Phí gửi xe máy', 70000),
-(6, 'Phí gửi xe oto', 1200000);
+(6, 'Phí gửi xe oto', 1200001);
 
 -- --------------------------------------------------------
 
@@ -317,7 +303,8 @@ CREATE TABLE `user_table` (
 --
 
 INSERT INTO `user_table` (`resident_id`, `user_username`, `user_password`) VALUES
-(9, 'phamvancuong', '$2y$10$5qIyi7UIU.Fs8JMcHS8fSeI3ZozzJfhLCwgAqVO3gxMl4/evar.9q');
+(1, 'theanh', '$2y$10$BecmLUCeA0kL3aDe8HxhnujimXwEfhuWMISGi.h7/nsz6WnXWaEX.'),
+(3, 'huuda', '$2y$10$t3m0LlJZBjc6msk/JM1bouLzTjzgD4uvSocGklI1F1S5E3Cq4a6U6');
 
 -- --------------------------------------------------------
 
@@ -338,13 +325,14 @@ CREATE TABLE `vehicles` (
 --
 
 INSERT INTO `vehicles` (`vehicle_id`, `resident_id`, `vehicle_name`, `vehicle_type`, `vehicle_plate`) VALUES
-(1, 1, 'Honda Wave trằng', 'Xe máy', '17B6-72685'),
-(2, 2, 'Honda Wave đen', 'Xe máy', '122345'),
-(3, 3, 'XYZ', 'Xe máy', 'XXXXXXXX'),
-(4, 9, 'VLXX', 'Xe máy', 'VLXX'),
-(5, 10, 'ĐCM', 'Xe máy', 'CCMNH'),
-(6, 15, '12225544', 'Xe máy', '5434653686'),
-(7, 11, 'CMMM', 'Xe máy', 'CMSNNSH');
+(4, 1, 'Honda Wave đen', 'Xe máy', '17B6-72685'),
+(6, 2, 'Exciter', 'Xe máy', '30B6-12345'),
+(7, 5, 'Audi Trắng', 'Oto', '30B-12345'),
+(8, 8, 'Lexus Trắng', 'Oto', '30B1-12345'),
+(9, 3, 'Camry Đỏ', 'Oto', '15A-12345'),
+(10, 9, 'Huyndai Tím', 'Oto', '15B-12345'),
+(11, 6, 'Yamaha Elegant Hồng', 'Xe máy', '30B5-12345'),
+(12, 5, 'Air Blade', 'Xe máy', '99A-9999');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -441,25 +429,25 @@ ALTER TABLE `apartments`
 -- AUTO_INCREMENT cho bảng `apartment_fees`
 --
 ALTER TABLE `apartment_fees`
-  MODIFY `apartment_fee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `apartment_fee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT cho bảng `fees`
 --
 ALTER TABLE `fees`
-  MODIFY `fee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `fee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT cho bảng `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT cho bảng `residents`
 --
 ALTER TABLE `residents`
-  MODIFY `resident_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `resident_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT cho bảng `tamtru`
@@ -471,7 +459,7 @@ ALTER TABLE `tamtru`
 -- AUTO_INCREMENT cho bảng `tamvang`
 --
 ALTER TABLE `tamvang`
-  MODIFY `tamvang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `tamvang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `type_fee`
@@ -483,29 +471,7 @@ ALTER TABLE `type_fee`
 -- AUTO_INCREMENT cho bảng `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- Các ràng buộc cho các bảng đã đổ
---
-
---
--- Các ràng buộc cho bảng `payments`
---
-ALTER TABLE `payments`
-  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`apartment_id`) REFERENCES `apartments` (`apartment_id`);
-
---
--- Các ràng buộc cho bảng `residents`
---
-ALTER TABLE `residents`
-  ADD CONSTRAINT `residents_ibfk_1` FOREIGN KEY (`apartment_id`) REFERENCES `apartments` (`apartment_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Các ràng buộc cho bảng `vehicles`
---
-ALTER TABLE `vehicles`
-  ADD CONSTRAINT `vehicles_ibfk_1` FOREIGN KEY (`resident_id`) REFERENCES `residents` (`resident_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `vehicle_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
